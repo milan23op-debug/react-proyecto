79c14e91e846a8a64de689d0ff46926fa5a44f97
milan <milan12op@gmail.com>
2025-11-25 21:10:49 -0600

    nueva version poke.0

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from "react";
+
+import React from "react";
 import Pokedex from "./Pokedex";
-import Spotify from "./Spotify";
 
 export default function App() {
-  const [pantalla, setPantalla] = useState("spotify");
-
   return (
     <div>
-      <nav style={{ marginBottom: "20px" }}>
-        <button onClick={() => setPantalla("spotify")}>Spotify</button>
-        <button onClick={() => setPantalla("pokedex")}>Pokédex</button>
-      </nav>
-
-      {pantalla === "spotify" && <Spotify />}
-      {pantalla === "pokedex" && <Pokedex />}
+      <Pokedex />
     </div>
   );
 }
